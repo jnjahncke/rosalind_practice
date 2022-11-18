9d11a56b1f516018ca432798699a87f13516e6a1
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-11-18 15:45:36 -0800

    remove extra loop

--- Locating_Restriction_Sites/palindrome.py
+++ Locating_Restriction_Sites/palindrome.py
@@ -20,24 +20,14 @@ with open(inputfile,"r") as inputfile:
 
 # scan through sequence looking for reverse palindromes
 palindromes = {} # location : sequence
-for i in range(len(sequence)):
-    for j in range(4,13):
-        for found in re.finditer(r"(?=([ATCG]{" + str(j) + r"}))", sequence[i:]):
-            s = found.group(1)
-            if reverse_complement(s) == s:
-                if found.start(1)+i+1 not in palindromes:
-                    palindromes[found.start(1)+i+1] = s
-                elif found.start(1)+i+1 in palindromes and len(s) > len(palindromes[found.start(1)+i+1]):
-                    palindromes[found.start(1)+i+1] = s
-
-#for j in range(4,13):
-#    for found in re.finditer(r"(?=([ATCG]{" + str(j) + r"}))", sequence):
-#        s = found.group(1)
-#        if reverse_complement(s) == s:
-#            if found.start(1)+1 not in palindromes:
-#                palindromes[found.start(1)+1] = s
-#            elif found.start(1)+1 in palindromes and len(s) > len(palindromes[found.start(1)+1]):
-#                palindromes[found.start(1)+1] = s
+for j in range(4,13):
+    for found in re.finditer(r"(?=([ATCG]{" + str(j) + r"}))", sequence):
+        s = found.group(1)
+        if reverse_complement(s) == s:
+            if found.start(1)+1 not in palindromes:
+                palindromes[found.start(1)+1] = s
+            elif found.start(1)+1 in palindromes and len(s) > len(palindromes[found.start(1)+1]):
+                palindromes[found.start(1)+1] = s
 
 # sort items by start location
 palindromes = {k: v for k, v in sorted(palindromes.items(), key=lambda item: item[0])}
