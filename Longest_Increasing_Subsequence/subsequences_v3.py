7c7bfd35cd4915a118848eaa7373592edfbaf624
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-11-21 15:49:15 -0800

    with array

--- Longest_Increasing_Subsequence/subsequences_v3.py
+++ Longest_Increasing_Subsequence/subsequences_v3.py
@@ -9,10 +9,10 @@ with open(inputfile,"r") as raw:
     perm = [int(x) for x in raw.readline().split()]
 
 def LIS(n,perm):
-    S = [[]]*(n+1)
+    arr = [[]]*(n+1)
     for i in perm:
-        S[i] = max(S[:i], key = len) + [i]
-    return(map(str, max(S, key = len)))
+        arr[i] = max(arr[:i], key = len) + [i]
+    return(map(str, max(arr, key = len)))
 
 print(*LIS(n,perm), sep = " ") # longest increasing sequence
 print(*list(LIS(n,reversed(perm)))[::-1], sep = " ") # longest decreasing sequence
