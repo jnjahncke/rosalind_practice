1c1973f56224c99fbba785f5d4a7c57eb71e4b70
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-11-21 11:11:29 -0800

    second version, still slow

--- Longest_Increasing_Subsequence/subsequences.py
+++ Longest_Increasing_Subsequence/subsequences.py
@@ -8,40 +8,36 @@ with open(inputfile,"r") as raw:
     n = int(raw.readline())
     perm = raw.readline().split()
 
-# find all increasing subsequences
+# find all increasing and decreasing subsequences
 increasing = []
-for i in range(len(perm)):
-    current = [perm[i]]
-    for j in range(i,len(perm)):
+decreasing = []
+for i in range(n):
+    current_inc = [perm[i]]
+    current_dec = [perm[i]]
+    for j in range(i,n):
         sub = perm[j:]
-        if len(sub) > 0 and perm[j] == min(sub) and perm[j] > current[-1]:
-                current.append(perm[j])
-    increasing.append(current)
-max_inc = max(increasing, key = len)
+        if len(sub) > 0 and perm[j] == min(sub) and perm[j] > current_inc[-1]:
+            current_inc.append(perm[j])
+        if len(sub) > 0 and perm[j] == max(sub) and perm[j] < current_dec[-1]:
+            current_dec.append(perm[j])
+    increasing.append(current_inc)
+    decreasing.append(current_dec)
 
-# check last digit
+max_inc = max(increasing, key = len)
+max_dec = max(decreasing, key = len)
+
+# check last digit increasing
 max_inc = max_inc[:-1]
 pos = perm.index(max_inc[-1])
-for i in range(pos,len(perm)):
+for i in range(pos,n):
     if perm[i] > max_inc[-1]:
         max_inc.append(perm[i])
 print(*max_inc, sep = " ")
 
-# find all decreasing subsequences
-decreasing = []
-for i in range(len(perm)):
-    current = [perm[i]]
-    for j in range(i,len(perm)):
-        sub = perm[j:]
-        if len(sub) > 0 and perm[j] == max(sub) and perm[j] < current[-1]:
-                current.append(perm[j])
-    decreasing.append(current)
-max_dec = max(decreasing, key = len)
-
-# check last digit
+# check last digit decreasing
 max_dec = max_dec[:-1]
 pos = perm.index(max_dec[-1])
-for i in range(pos,len(perm)):
+for i in range(pos,n):
     if perm[i] < max_dec[-1]:
         max_dec.append(perm[i])
 print(*max_dec, sep = " ")
