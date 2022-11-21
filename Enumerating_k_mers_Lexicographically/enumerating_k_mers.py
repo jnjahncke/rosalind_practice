644f65de12ddef8b2c0ae09e3be180f318d2cf1f
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-11-20 17:49:17 -0800

    all possible combos in alphabetical order

---
+++ Enumerating_k_mers_Lexicographically/enumerating_k_mers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+from random import randrange
+from copy import deepcopy
+
+inputfile = sys.argv[1]
+
+with open(inputfile,"r") as raw:
+    alphabet, n = raw.readlines()
+
+alphabet = alphabet.rstrip().split()
+n = int(n)
+
+possible = len(alphabet)**n
+
+strings = []
+for i in range(len(alphabet)):
+    menu = deepcopy(alphabet)
+    while len(menu) > 0:
+        current = alphabet[i]
+        while len(current) < n:
+            current += menu[0]
+            menu.pop(0)
+        strings.append(current)    
+print(*strings, sep = "\n")
