a46d9ea62659621bd4fbb15f01d3d66f0348d1f1
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-12-28 11:45:18 -0800

    too slow

--- Speeding_Up_Motif_Finding/motif.py
+++ Speeding_Up_Motif_Finding/motif.py
@@ -9,16 +9,6 @@ def motif(seq, subseq):
     else:
         return False
 
-def memoize(func):
-    cache = {}
-    def decorated_function(*args):
-        if args not in cache:
-            cache[args] = func(*args)
-        return cache[args]
-    return decorated_function
-
-motif = memoize(motif)
-
 # import sequence
 seq = ""
 with open(sys.argv[1],"r") as fasta:
@@ -31,10 +21,12 @@ with open(sys.argv[1],"r") as fasta:
 array = [0]
 for i in range(1,len(seq)):
     longest = 0
-    for j in range(i+1):
+    for j in reversed(range(i+1)):
         newseq = seq[:i]
         subseq = seq[i-j:i+1]
-        if subseq[0] == seq[0]:
+        if len(subseq) < longest:
+            break
+        elif subseq[0] == seq[0]:
             find = motif(newseq, subseq)
             if find == True:
                 length = len(subseq) 
