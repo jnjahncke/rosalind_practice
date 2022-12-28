d41665465fb8be57238b5dd20c1f1b5d57d3bc26
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-12-28 11:16:52 -0800

    working code but too slow

---
+++ Speeding_Up_Motif_Finding/README.md
@@ -0,0 +1,22 @@
+# Problem
+
+A prefix of a length n string s is a substring s[1:j]; a suffix of s is a substring s[k:n].
+
+The failure array of s is an array P of length n for which P[k] is the length of the longest substring s[j:k] that is equal to some prefix s[1:k−j+1], where j cannot equal 1 (otherwise, P[k] would always equal k). By convention, P[1]=0.
+
+**Given**: A DNA string s (of length at most 100 kbp) in FASTA format.
+
+**Return**: The failure array of s.
+
+## Sample Dataset
+
+```
+>Rosalind_87
+CAGCATGGTATCACAGCAGAG
+```
+
+## Sample Output
+
+```
+0 0 0 1 2 0 0 0 0 0 0 1 2 1 2 3 4 5 3 0 0
+```
---
+++ Speeding_Up_Motif_Finding/motif.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+
+def motif(seq, subseq):
+    result = seq.find(subseq)
+    if result == 0:
+        return True
+    else:
+        return False
+
+# import sequence
+seq = ""
+with open(sys.argv[1],"r") as fasta:
+    for line in fasta:
+        line = line.rstrip()
+        if line[0] != ">":
+            seq += line
+
+# build failure array
+array = [0]
+for i in range(1,len(seq)):
+    longest = 0
+    for j in range(i+1):
+        newseq = seq[:i]
+        subseq = seq[i-j:i+1]
+        if subseq[0] == seq[0]:
+            find = motif(newseq, subseq)
+            if find == True:
+                length = len(subseq) 
+                if length > longest:
+                    longest = length
+    array.append(longest)
+print(*array, sep = " ")
