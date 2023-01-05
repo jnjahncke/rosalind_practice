bddcf71429d4858a994e68ce2f5029f3e2e798de
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2023-01-05 12:02:56 -0800

    too slow

--- Finding_a_Shared_Spliced_Motif/lcsq.py
+++ Finding_a_Shared_Spliced_Motif/lcsq.py
@@ -4,11 +4,11 @@ import sys
 from itertools import combinations
 
 def is_subseq(seq, pattern):
+    i = 0
     for NT in pattern:
-        loc = seq.find(NT)
-        if loc >= 0:
-            seq = seq[loc+1:]
-        else:
+        try:
+            i = i + seq[i:].index(NT) + 1
+        except:
             return False
     return True
 
@@ -24,12 +24,13 @@ with open(sys.argv[1],"r") as fasta:
             seq_dict[seq_name] += line
 seq1, seq2 = seq_dict.values()
 
-# find all possible subsequences of seq1
+# use combinations to find all subsequences of seq1
+# starting from longest, subsequences, 
+# see if they are also subsequences of seq2
+# break at first match
 seq1_sub = []
 for length in range(len(seq1),0,-1):
-    temp = [x for x in combinations(seq1, length)]
-    for x in temp:
-        x = "".join(x)
+    for x in ["".join(x) for x in combinations(seq1, length)]:
         if x not in seq1_sub:
             seq1_sub.append(x)
             if is_subseq(seq2, x) == True:
