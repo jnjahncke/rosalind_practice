2be77dff2f61876c0e87da2bf99f168ecb54c047
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2023-01-05 11:48:33 -0800

    too slow

--- Finding_a_Shared_Spliced_Motif/lcsq.py
+++ Finding_a_Shared_Spliced_Motif/lcsq.py
@@ -26,19 +26,12 @@ seq1, seq2 = seq_dict.values()
 
 # find all possible subsequences of seq1
 seq1_sub = []
-for length in range(1,len(seq1)):
+for length in range(len(seq1),0,-1):
     temp = [x for x in combinations(seq1, length)]
     for x in temp:
         x = "".join(x)
         if x not in seq1_sub:
             seq1_sub.append(x)
-
-# check to see which are also subsequences of seq2
-both = []
-for subseq in seq1_sub:
-    if is_subseq(seq2, subseq) == True:
-        both.append(subseq)
-
-# sort both list by length, take longest subseq
-both.sort(reverse = True, key = len)
-print(both[0])
+            if is_subseq(seq2, x) == True:
+                print(x)
+                exit()
