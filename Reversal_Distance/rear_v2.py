89e3237b01013037ed0b2f8e708dc54ca3f28821
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2023-01-25 17:10:42 -0800

    working code using greedy algorithm

--- Reversal_Distance/rear_v2.py
+++ Reversal_Distance/rear_v2.py
@@ -9,9 +9,9 @@ def breakpoints(k,g):
     l = len(k)-1
     # compare sequences
     for n in range(l):
-        k1 = k[n]
-        k2 = k[n+1]
-        if abs(g.index(k1) - g.index(k2)) != 1: 
+        g1 = g[n]
+        g2 = g[n+1]
+        if abs(k.index(g1) - k.index(g2)) != 1: 
             breakpoints.append(n+1)
     return(sorted(breakpoints))
 
@@ -29,23 +29,13 @@ def rev_combos(k,seq_list):
     bp_list = []
     for seq in seq_list:
         bps = breakpoints(k,seq)
-        if len(bps) > 0:
-            combos = combinations(bps,2)
-            for start,stop in combos:
-                rev = reversal(seq,start,stop)
-                new_seq_list.append(rev)
-                bp_list.append(len(breakpoints(k,rev)))
-                print(f"Key: {k}\nSeq: {seq}\nBPs: {bps}\nStart: {start}\tEnd: {stop}\nRev: {rev}\nNew BPs: {breakpoints(k,rev)}")
-        else:
-            return([seq])
-    print(f"New seq list: {new_seq_list}")
-    print(f"BP list: {bp_list}")
-    if len(bp_list) > 0:
-        min_bp = min(bp_list)
-        w_min_bp = [x for x in new_seq_list if len(breakpoints(k,x)) == min_bp]
-    else:
-        w_min_bp = new_seq_list
-    print(f"With min bps: {w_min_bp}")
+        combos = combinations(bps,2)
+        for start,stop in combos:
+            rev = reversal(seq,start,stop)
+            new_seq_list.append(rev)
+            bp_list.append(len(breakpoints(k,rev)))
+    min_bp = min(bp_list)
+    w_min_bp = [x for x in new_seq_list if len(breakpoints(k,x)) == min_bp]
     return(w_min_bp)
 
 # import sequences
@@ -68,15 +58,12 @@ with open(sys.argv[1],"r") as perms:
 perms = zip(keys,given)
 result = []
 for k,g in perms:
-    k.insert(0,"a")
-    k.append("b")
-    g.insert(0,"a")
-    g.append("b")
+    k = ["a"] + k + ["b"]
+    g = ["a"] + g + ["b"]
     change = 0
     rev = [g]
     while k not in rev:
         rev = rev_combos(k,rev)
         change += 1
-        print(f"Change: {change}")
     result.append(change)
 print(*result)
