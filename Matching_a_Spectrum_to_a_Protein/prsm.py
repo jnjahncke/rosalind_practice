094d94d4c16fac4bac40fd2714796d337762c0f6
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2023-03-06 16:57:06 -0800

    not working yet

--- Matching_a_Spectrum_to_a_Protein/prsm.py
+++ Matching_a_Spectrum_to_a_Protein/prsm.py
@@ -36,19 +36,32 @@ for p in peptides:
         if y_mass > 0:
             ion_dict[p].append(round(y_mass,5))
 
-# find all possible differences between ions
+# find all possible differences between ions in spectrum
+diff = []
+for i,j in combinations(spec,2):
+    diff.append(round(abs(i-j),5))
+spec += diff
+
+# find all possible differences between ions in peptides
+#temp = []
 #for p in peptides:
-    temp = []
-    for i,j in combinations(ion_dict[p],2):
-        temp.append(round(abs(i-j),5))
-    ion_dict[p] += temp
+#    for i,j in combinations(ion_dict[p],2):
+#        temp.append(round(abs(i-j),5))
+#    ion_dict[p] += temp
 
 # calculate multiplicities
 mult = {}
 for p in peptides:
     mult[p] = 0
-    for ion in spec:
+    for ion in diff:
         if round(ion,5) in ion_dict[p]:
             mult[p] += 1
 
-print(mult)
+max_mult = {"x":0}
+for p in mult:
+    if mult[p] > list(max_mult.values())[0]:
+        max_mult = {p:mult[p]}
+
+for k,v in max_mult.items():
+    print(v)
+    print(k)
